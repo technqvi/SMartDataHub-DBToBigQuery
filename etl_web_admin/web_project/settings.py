ec598d16a366eb23fdf1b10fe514a5e7855ce285
technqvi <technqvi_gangster@hotmail.com>
2024-01-15 21:14:02 +0700

    ok

--- etl_web_admin/web_project/settings.py
+++ etl_web_admin/web_project/settings.py
@@ -25,7 +25,7 @@ env = environ.Env(
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
