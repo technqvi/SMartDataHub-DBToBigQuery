a2d0dbc512bfabfc78f733dc4696dfdda9c654ac
technqvi <technqvi_gangster@hotmail.com>
2024-01-15 20:53:40 +0700

    ok

--- etl_web_admin/app/admin.py
+++ etl_web_admin/app/admin.py
@@ -1,62 +1,45 @@
 from django.contrib import admin
 from .models import  *
+from django.contrib.admin import SimpleListFilter
 # Register your models here.
 
-@admin.register(DataSource)
-class DataSourceAdmin(admin.ModelAdmin):
+@admin.register(ViewSource)
+class ViewSourceAdmin(admin.ModelAdmin):
     actions =None
-    list_display = ['id','load_from_type','datastore','partition_date_col']
-    readonly_fields = ('id',)
-    search_fields = ['id']
+    list_display = ['id','name']
+    search_fields = ['name']
 
     def has_delete_permission(self, request, obj=None):
         return False
-    
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            return ['id']
-        else:
-            return []
-    
-@admin.register(DataStore)
-class DataStoreAdmin(admin.ModelAdmin):
-    actions =None
-    list_display = ['data_store_name','database_ip','database_host','database_port']
-    readonly_fields = ('data_store_name',)
-    search_fields = ['data_store_name']
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-    
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            return ['data_store_name']
-        else:
-            return []
-    
+class ViewSourceFilter(SimpleListFilter):
+    title = "View Source"
+    parameter_name = 'view_source'
 
+    def lookups(self, request, model_admin):
+        sourceList = set([c.view_source  for c in model_admin.model.objects.select_related('view_source').all()])
+        return [(c.id, c.name) for c in sourceList]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            try:
+                view_source_id = int(self.value())
+            except (ValueError):
+                return queryset.none()
+            else:
+                return queryset.filter(view_source__id=view_source_id ) 
     
 @admin.register(ETLTransaction)
-class ETLTransactionAdmin(admin.ModelAdmin):
-    actions = None
-    list_display = ['id','etl_datetime','data_source_id','completely','is_load_all']
-    search_fields = ['data_source_id','etl_datetime']
-    def has_delete_permission(self, request, obj=None):
-        return False
+class ETLTransaction(admin.ModelAdmin):
+    actions =None
+    list_display = ['trans_datetime','view_source','type','no_rows','is_consistent','is_complete']
+    search_fields =['trans_datetime']
+    list_filter = (ViewSourceFilter,)
+
     def has_add_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
         return False
     def has_change_permission(self, request, obj=None):
         return  False
-
-@admin.register(LogError)
-class LogErrorAdmin(admin.ModelAdmin):
-    actions = None
-    list_display = ['id','error_datetime','etl_datetime','data_source_id']
-    search_fields = ['error_datetime','etl_datetime','data_source_id']
-    def has_delete_permission(self, request, obj=None):
-        return False
-    def has_add_permission(self, request, obj=None):
-        return False
-    def has_change_permission(self, request, obj=None):
-        return  False
-
+    
--- etl_web_admin/app/models.py
+++ etl_web_admin/app/models.py
@@ -3,112 +3,26 @@ from datetime import datetime
 
 # Create your models here.
 
-
-DATABASE_PRODUCT_CHOICES = (
-    ("oracle","Oracle"),
-    ("mssql","MSSQL" ),
-     ("postgresql","Postgresql"),
-)
-
-
-class DataStore(models.Model):
-
-    database_ip=models.CharField(max_length=255,verbose_name='IP')
-    database_host=models.CharField(max_length=255,verbose_name='HOST NAME') 
-    database_port=models.IntegerField(verbose_name='PORT')
-
-     
-    databases_service_name=models.CharField(max_length=255,verbose_name='SERVICE / INSTANCE NAME') 
-    
-    databases_user=models.CharField(max_length=255,verbose_name='USER')
-    databases_password=models.CharField(max_length=255,verbose_name='PASSWORD')
-
-    data_store_name=models.CharField(max_length=255,verbose_name='DATA STORE NAME',unique=True)
-    
-    
-    database_product=  models.CharField(
-        max_length = 50,
-        choices = DATABASE_PRODUCT_CHOICES,
-        default = '1',verbose_name='DATABASE PRODUCT'
-    )
-    
+class ViewSource(models.Model):
+    name = models.CharField(max_length=255)
     class Meta:
         managed = False
-        db_table = 'data_store'
-
+        db_table = 'view_source'
+        ordering = ['id']
     def __str__(self):
-        return self.data_store_name
-    
-
-PARTITIONING_CHOICES = (
-    ("DAY", "DAY"),
-    ("MONTH", "MONTH"),
-    ("YEAR", "YEAR"),
-)
-
-LOAD_TO_BQ_FROM_CHOICES = (
-    ("dataframe", "dataframe"),
-    ("csv", "csv"),
-)
-
-class DataSource(models.Model):
-    #type_name = models.CharField(unique=True, max_length=255,editable=False)
-    id = models.CharField(primary_key=True, max_length=100,verbose_name="View Name")
-    datastore = models.ForeignKey(DataStore,  on_delete=models.CASCADE)
-    first_load_col=models.CharField(max_length=255,verbose_name='First Load Column',help_text='Date Column to determine condition to load since ?')
-    first_load_date= models.DateField('First Load Date',default=datetime(2020,1,1),help_text="Date for the first full load")  
-    partition_date_col=models.CharField(max_length=100,verbose_name='Partition Column',help_text='only 1 colName')
-    partition_date_type=  models.CharField(
-        max_length = 50,
-        choices = PARTITIONING_CHOICES,
-        default = '2',verbose_name='Partition Type'
-    )
-    load_from_type=  models.CharField(
-        max_length = 50,
-        choices = LOAD_TO_BQ_FROM_CHOICES,
-        default = '1',verbose_name='Load To BQ From Type'
-    )
-    
-    cluster_col_list=models.TextField(max_length=255,null=True,blank=True,verbose_name= 'Cluster List(4 Cols)',help_text='colName1,colName2,colName3,colName4')
-    date_col_list=models.TextField(max_length=255,null=True,blank=True,verbose_name= 'DateColumn List',help_text='colName1,colName2...')
-    
-    
-    class Meta:
-        managed = False
-        db_table = 'data_source'
-
-    def __str__(self):
-        return self.id
-    
+     return self.name
     
 class ETLTransaction(models.Model):
-    #type_name = models.CharField(unique=True, max_length=255,editable=False)
-    id = models.IntegerField(primary_key=True)
-    etl_datetime = models.CharField(max_length=100)
-    data_source_id=models.CharField(max_length=100)
-    # data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
-    completely=models.IntegerField()
-    is_load_all=models.IntegerField()
+
+    trans_datetime = models.CharField(max_length=100)
+    view_source = models.ForeignKey(ViewSource, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
+    no_rows=models.IntegerField()
+    is_consistent=models.IntegerField()
+    is_complete=models.IntegerField()
     class Meta:
         managed = False
         db_table = 'etl_transaction'
         ordering = ['-id']
 
-    def __str__(self):
-        return f"{self.data_source_id} # {self.etl_datetime}"
     
-class LogError(models.Model):
-    #type_name = models.CharField(unique=True, max_length=255,editable=False)
-    id = models.IntegerField(primary_key=True)
-    error_datetime = models.CharField(max_length=100)
-    etl_datetime = models.CharField(max_length=100)
-    data_source_id=models.CharField(max_length=100)
-    # data_source = models.ForeignKey(DataSource, on_delete=models.CASCADE)
-    message =models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'log_error'
-        ordering = ['-id']
-    def __str__(self):
-        return f"{self.etl_datetime} # {self.error_datetime} # {self.data_source_id}"
