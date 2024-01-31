78ae5f459d0f9a605ba332c087b5bbccd746854a
technqvi <technqvi_gangster@hotmail.com>
2024-02-01 06:09:47 +0700

    ok

--- table_schema_script/check_log.sql
+++
@@ -1,5 +0,0 @@
- SELECT t.*
-                FROM public.models_logging_change t
-                WHERE date_created>='2024-01-12 21:00' and content_type_id in (36,37,14,7)
-                ORDER BY date_created desc
-                LIMIT 501
